b16f2d0bd82072f73757efcb5e6cab7812f9af7b
sncot <sncot123@aliyun.com>
2022-07-14 18:58:31 +0800

    add dataStruct

--- go_basic/day01/for07/main.go
+++ go_basic/day01/for07/main.go
@@ -3,23 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	//for循环    go语言中循环只有这一种
+	//for循环    go语言中循环只有for这一种
 
-	for i:=0;i<10;i++ {
-		fmt.Printf("%d\n",i)
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d\n", i)
 	}
 
 	//变种1
-	a:=0
-	for ;a<10;a++ {
-		fmt.Printf("%d\n",a)
+	a := 0
+	for ; a < 10; a++ {
+		fmt.Printf("%d\n", a)
 	}
 
 	//变种2
 
-	b:=0
-	for b < 10{
-		fmt.Printf("%d\n",b)
+	b := 0
+	for b < 10 {
+		fmt.Printf("%d\n", b)
 		b++
 	}
 
@@ -28,29 +28,29 @@ func main() {
 	// 	fmt.Println("我好想你！潘丽萍")
 	// }
 
-	for i:=0;i<10;i++ {
-		if i == 5{//当i==5时跳出循环		
+	for i := 0; i < 10; i++ {
+		if i == 5 { //当i==5时跳出循环
 			break
 		}
-		fmt.Printf("%d\n",i)
+		fmt.Printf("%d\n", i)
 	}
 
-	for i:=0;i<10;i++ {
-		if i ==5{//当i==5时跳过此次循环
+	for i := 0; i < 10; i++ {
+		if i == 5 { //当i==5时跳过此次循环
 			continue
 		}
-		fmt.Printf("%d\n",i)
+		fmt.Printf("%d\n", i)
 	}
 
-	for i:=0;i<10;i++ {
-		for j:=0;j<10;j++{
-			if i==3 && j==5{
-				goto XX	//当条件满足时，跳出循环到XX标签处
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			if i == 3 && j == 5 {
+				goto XX //当条件满足时，跳出循环到XX标签处
 			}
-			fmt.Printf("%d->%d\n",i,j)
+			fmt.Printf("%d->%d\n", i, j)
 
 		}
 	}
-	XX:	//Label标签
-		fmt.Print("over")
+XX: //Label标签
+	fmt.Print("over")
 }
--- go_basic/day13/test/main.go
+++ go_basic/day13/test/main.go
@@ -1,41 +1,19 @@
 package main
 
-import (
-	"context"
-	"fmt"
-	"go.etcd.io/etcd/clientv3"
-	"time"
-)
+import "fmt"
 
 func main() {
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"127.0.0.1:2379"},
-		DialTimeout: 5 * time.Second,
-	})
-
-	if err != nil {
-		fmt.Printf("connect to etcd failed! err: %v\n", err)
-		return
+	s := "asdfghjas"
+	l := len(s)
+	b := 0
+	for i := 0; i < l-b; i++ {
+		for j := i + 1; j < l; j++ {
+			if s[i] == s[j] {
+				if b < j-i {
+					b = b - i
+				}
+			}
+		}
 	}
-	fmt.Println("connect to etcd succeeded!")
-	defer cli.Close()
-	//put
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	_, err = cli.Put(ctx, "zhou", "love")
-	cancel()
-	if err != nil {
-		fmt.Printf("put to etcd failed! err: %v\n", err)
-		return
-	}
-	//get
-	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
-	value := `[{"path":"d:/k-v/tmp/etcd.log","topic":"etcd_log"},
-               {"path":"d:/k_v/tmp/kafka.log","topic":"kafka_log"},
-               {"path":"d:/k_v/tmp/redis.log","topic":"redis_log"}]`
-	_, err = cli.Put(ctx, "/collect/config", value)
-	cancel()
-	if err != nil {
-		fmt.Printf("get from etcd failed ! err:%v\n", err)
-		return
-	}
+	fmt.Println(b)
 }
