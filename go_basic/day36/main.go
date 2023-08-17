688e1e6d9884f5b47ef98c96856bcd544f4d554e
nuts <nuts_plp@126.com>
2023-08-17 12:53:39 +0800

    再次添加一些变动

--- go_basic/day36/main.go
+++ go_basic/day36/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+	engine.GET("/898989", func(c *gin.Context) {
+		c.JSONP(http.StatusOK, gin.H{
+			"hello": "asjdakjfas",
+			"sfjs":  12,
+		})
+
+	})
+	engine.Run(":8000")
 }
